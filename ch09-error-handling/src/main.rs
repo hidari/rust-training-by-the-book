bddd761e845e87854f92b813cd458df098f524d7
Hidari <ika5140+github@gmail.com>
2023-01-21 16:36:58 +0900

    エラーの移譲

--- ch09-error-handling/src/main.rs
+++ ch09-error-handling/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io;
+use std::io::{ErrorKind, Read};
 
 fn main() {
     let f = File::open("hello.txt");
@@ -17,4 +18,20 @@ fn main() {
             panic!("There was a problem opening the file: {:?}", error)
         }
     };
+}
+
+fn read_username_from_file() -> Result<String, io::Error>{
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
