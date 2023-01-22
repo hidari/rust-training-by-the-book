e8b038ba22346d7e7915c9e19214d6a185798c6f
Hidari <ika5140+github@gmail.com>
2023-01-22 17:16:17 +0900

    traitはじめ

--- ch10-generics-trait-lifetime/src/main.rs
+++ ch10-generics-trait-lifetime/src/main.rs
@@ -6,6 +6,15 @@ fn main() {
     let number_list = vec!['y', 'm', 'a', 'q'];
     let result = largest_char(&number_list);
     println!("The largest char is {}", result);
+
+    let tweet = Tweet{
+        username: String::from("Hidari0415"),
+        content: String::from("銀髪は至高。"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize())
 }
 
 fn largest_i32(list: &[i32]) -> i32{
@@ -42,3 +51,33 @@ fn largest_char(list: &[char]) -> char {
 //
 //     largest
 // }
+
+pub trait Summary{
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle{
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle{
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet{
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
