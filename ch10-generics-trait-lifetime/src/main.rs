135c679dd85ac8e717a32ac6f61196d9d5239e76
Hidari <ika5140+github@gmail.com>
2023-01-22 17:05:19 +0900

    ジェネリック（仮）

--- ch10-generics-trait-lifetime/src/main.rs
+++ ch10-generics-trait-lifetime/src/main.rs
@@ -31,3 +31,14 @@ fn largest_char(list: &[char]) -> char {
 
     largest
 }
+
+// fn largest<T>(list: &[T]) -> T{
+//     let mut largest = list[0];
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//
+//     largest
+// }
