b13a59ee6be5926d9398f90b9b8333311731b06f
Hidari <ika5140+github@gmail.com>
2023-01-25 19:35:28 +0900

    構造体のライフタイム注釈

--- ch10-generics-trait-lifetime/src/main.rs
+++ ch10-generics-trait-lifetime/src/main.rs
@@ -34,6 +34,12 @@ fn main() {
         let result = longest(string1.as_str(), string2);
         println!("The longest string is {}", result);
     }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
 
 fn largest_i32(list: &[i32]) -> i32 {
@@ -76,5 +82,20 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         x
     } else {
         y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
     }
 }
