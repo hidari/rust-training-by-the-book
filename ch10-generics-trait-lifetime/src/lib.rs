eb63a4c9a2a788730d643dad9160bae9c125eb0a
Hidari <ika5140+github@gmail.com>
2023-01-25 18:31:26 +0900

    メソッド実装の条件分け

--- ch10-generics-trait-lifetime/src/lib.rs
+++ ch10-generics-trait-lifetime/src/lib.rs
@@ -52,3 +52,24 @@ pub fn returns_summarizable() -> impl Summary {
         retweet: false,
     }
 }
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
