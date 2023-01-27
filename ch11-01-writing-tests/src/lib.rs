1a5883872ee7b4f5c6ccf4dc104f8196f33ea480
Hidari <ika5140+github@gmail.com>
2023-01-27 20:38:38 +0900

    assert!マクロに失敗時のメッセージを追加

--- ch11-01-writing-tests/src/lib.rs
+++ ch11-01-writing-tests/src/lib.rs
@@ -6,6 +6,10 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello!")
+}
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -64,5 +68,15 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Hidari");
+        assert!(
+            result.contains("Hidari"),
+            "Greeting did not contain name, value was '{}'",
+            result
+        );
     }
 }
