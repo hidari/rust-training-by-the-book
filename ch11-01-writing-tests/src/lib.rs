c90243a9f4c33aa83a63cd7bdcd31c13d98ad693
Hidari <ika5140+github@gmail.com>
2023-01-28 16:40:46 +0900

    --test-threadsと--nocaptureの動作を確認

--- ch11-01-writing-tests/src/lib.rs
+++ ch11-01-writing-tests/src/lib.rs
@@ -44,6 +44,11 @@ impl Guess {
     }
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -115,5 +120,18 @@ mod tests {
         } else {
             Err(String::from("two plus two does not equal four"))
         }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    #[ignore]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
