55a0e63d487dbfd7635ef5efca1724e1f876e055
Hidari <ika5140+github@gmail.com>
2023-02-04 15:53:24 +0900

    不要な関数を削除

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -32,10 +32,3 @@ impl Config {
         Config { query, filename }
     }
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
