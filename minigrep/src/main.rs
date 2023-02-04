2b5bfa59b4c58e5123f080d59f9901bd3570bdc4
Hidari <ika5140+github@gmail.com>
2023-02-04 16:44:30 +0900

    run関数に処理を隔離

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,6 @@
-use std::{env, process};
 use std::fs::File;
 use std::io::prelude::*;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,6 +13,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
@@ -28,9 +32,9 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str>  {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return  Err("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
