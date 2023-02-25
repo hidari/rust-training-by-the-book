fdc669def7984ddfd666c93f4638dfac6296ad29
Hidari <ika5140+github@gmail.com>
2023-02-25 16:34:43 +0900

    Iteratorを実装

--- ch13-00-functional-features/src/lib.rs
+++ ch13-00-functional-features/src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod test {
+    use crate::Counter;
+
     #[test]
     fn iterator_demonstration() {
         let v1 = vec![1, 2, 3];
@@ -69,5 +71,41 @@ mod test {
                 },
             ]
         )
+    }
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
     }
 }
