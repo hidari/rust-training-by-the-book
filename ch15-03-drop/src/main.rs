eede2660c4d36522162c1c091aae0ffd4ddb5479
Hidari <ika5140+github@gmail.com>
2023-03-04 17:11:50 +0900

    drop関数の使用

--- ch15-03-drop/src/main.rs
+++ ch15-03-drop/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let c = CustomSmartPointer { data: String::from("my stuff") };
-    let d = CustomSmartPointer { data: String::from("other stuff") };
+    let c = CustomSmartPointer { data: String::from("some data") };
+    // let d = CustomSmartPointer { data: String::from("other stuff") };
+    drop(c);
     println!("CustomSmartPointers created.");
 }
 
