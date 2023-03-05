f73b78b9e3a9452813a12da01e7e41e1e502a379
Hidari <ika5140+github@gmail.com>
2023-03-05 17:08:28 +0900

    コンスリストをRefCell<T>で改変

--- ch15-04-rc/src/main.rs
+++ ch15-04-rc/src/main.rs
@@ -1,22 +1,26 @@
+extern crate core;
+
+#[derive(Debug)]
+enum List{
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use std::cell::RefCell;
 use std::rc::Rc;
 use crate::List::{Cons, Nil};
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil))))) ;
-    println!("count after creating a = {}", Rc::strong_count(&a));
+    let value = Rc::new(RefCell::new(5));
 
-    let _b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
 
-    {
-        let _c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
+    *value.borrow_mut() += 10;
 
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
-}
 
-enum List{
-    Cons(i32, Rc<List>),
-    Nil,
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
