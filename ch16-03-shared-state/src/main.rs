87a1105bf81afe9b721237008207a20631c0b184
Hidari <ika5140+github@gmail.com>
2023-03-06 19:05:12 +0900

    複数スレッドでMutex

--- ch16-03-shared-state/src/main.rs
+++ ch16-03-shared-state/src/main.rs
@@ -1,29 +1,19 @@
-use std::sync::Mutex;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    let counter = Mutex::new(0);
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
-    // for _ in 1..10 {
-    //     let handle = thread::spawn(move || {
-    //         let mut num = counter.lock().unwrap();
-    //         *num += 1;
-    //     });
-    //     handles.push(handle);
-    // }
-
-    let handle = thread::spawn(move || {
-        let mut num = counter.lock().unwrap();
-        *num += 1;
-    });
-    handles.push(handle);
-
-    let handle2 = thread::spawn(move || {
-        let mut num2 = counter.lock().unwrap();
-        *num2 += 1;
-    });
-    handles.push(handle2);
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
 
     for handle in handles {
         handle.join().unwrap();
