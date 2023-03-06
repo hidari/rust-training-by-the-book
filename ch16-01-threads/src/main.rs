dc19df9749cdc2fcad30bcf2b8280221b19d0981
Hidari <ika5140+github@gmail.com>
2023-03-06 14:14:02 +0900

    threadのクロージャにmoveさせるやつ

--- ch16-01-threads/src/main.rs
+++ ch16-01-threads/src/main.rs
@@ -15,4 +15,13 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    println!("===========================");
+
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap()
 }
