063bb233a009e4867769ca66b4f8efb4d2ee65c0
Hidari <ika5140+github@gmail.com>
2023-03-08 16:33:53 +0900

    パターンが所有権を奪わないように参照を得る

--- ch18-03-pattern-syntax/src/main.rs
+++ ch18-03-pattern-syntax/src/main.rs
@@ -106,5 +106,14 @@ fn main() {
     match numbers {
         (first, .., last) => println!("Some numbers: {}, {}", first, last),
     }
+
+    println!("========================");
+
+    let robot_name = Some(String::from("Bors"));
+    match robot_name {
+        Some( ref name) => println!("Found a name: {}", name),
+        None => (),
+    }
+    println!("robot_name is : {:?}", robot_name);
 }
 
