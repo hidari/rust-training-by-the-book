f0a0d5fd2ec57b2b533ac9898426a45b8edd4142
Hidari <ika5140+github@gmail.com>
2023-03-08 17:44:19 +0900

    unsafeなコードをsafe rustに抽象化

--- ch19-01-unsafe-rust/src/main.rs
+++ ch19-01-unsafe-rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     let mut num = 5;
     let r1 = &num as *const i32;
@@ -10,4 +12,25 @@ fn main() {
 
     let address = 0x012345usize;
     let _r = address as *const i32;
+
+    println!("========================");
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (slice::from_raw_parts_mut(ptr, mid),
+        slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid))
+    }
 }
