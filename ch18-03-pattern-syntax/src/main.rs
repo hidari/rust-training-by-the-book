557ee90f315cd29108e82d17aa57dad04558767c
Hidari <ika5140+github@gmail.com>
2023-03-08 16:40:55 +0900

    マッチガード

--- ch18-03-pattern-syntax/src/main.rs
+++ ch18-03-pattern-syntax/src/main.rs
@@ -115,5 +115,14 @@ fn main() {
         None => (),
     }
     println!("robot_name is : {:?}", robot_name);
+
+    println!("========================");
+
+    let num = Some(4);
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
 }
 
