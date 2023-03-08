8b7d51d04efd342b58b96276c6e19b72cc29cb01
Hidari <ika5140+github@gmail.com>
2023-03-08 15:28:59 +0900

    構造体の分配

--- ch18-03-pattern-syntax/src/main.rs
+++ ch18-03-pattern-syntax/src/main.rs
@@ -44,5 +44,13 @@ fn main() {
 
     println!("========================");
 
-    
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point{x: 0, y: 7};
+    let Point{x: a, y: b} = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
 }
