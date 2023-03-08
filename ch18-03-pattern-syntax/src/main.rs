df40d1c7066b56434df590f641f409c6cfea2086
Hidari <ika5140+github@gmail.com>
2023-03-08 15:50:57 +0900

    参照の分配

--- ch18-03-pattern-syntax/src/main.rs
+++ ch18-03-pattern-syntax/src/main.rs
@@ -76,4 +76,16 @@ fn main() {
         Message::Write(text) => println!("Text message: {}", text),
         Message::ChangeColor(r, g, b) => println!("Change the color to red {}, green {}, blue {}", r, g, b),
     }
+
+    println!("========================");
+
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+    let sum_of_squares: i32 = points
+        .iter()
+        .map(|&Point { x, y }| x * x + y * y)
+        .sum();
 }
