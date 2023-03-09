d1c75a718151bb2b29e44cb88c5135b12b3c68e8
Hidari <ika5140+github@gmail.com>
2023-03-09 16:29:24 +0900

    通常は直接実装されたメソッドが呼ばれる

--- ch19-03-advanced-traits/src/main.rs
+++ ch19-03-advanced-traits/src/main.rs
@@ -28,7 +28,35 @@ impl Add<Meters> for Millimeters {
     }
 }
 
+trait Pilot{
+    fn fly(&self);
+}
+trait Wizard{
+    fn fly(&self);
+}
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+impl Wizard for Human{
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human{
+    fn fly(&self){
+        println!("*waving arm furiously*");
+    }
+}
+
 fn main() {
     assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
                Point { x: 3, y: 3 });
+
+    let person = Human;
+    person.fly();
 }
