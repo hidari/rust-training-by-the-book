9410a1f6575a5f1d1c81beef8abc25ea6c1c0daf
Hidari <ika5140+github@gmail.com>
2023-03-10 15:46:02 +0900

    ニュータイプパターン

--- ch19-03-advanced-traits/src/main.rs
+++ ch19-03-advanced-traits/src/main.rs
@@ -96,6 +96,15 @@ impl fmt::Display for Point{
     }
 }
 
+// ===========================
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.join(", "))
+    }
+}
+
 fn main() {
     assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
                Point { x: 3, y: 3 });
@@ -109,4 +118,7 @@ fn main() {
 
     let point = Point { x: 3, y: 4 };
     point.outline_print();
+
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
